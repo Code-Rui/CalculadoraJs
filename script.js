e17959d08cb195abe6e2ea83a380963079acc44f
jjjjose <jjjjoseignacio@gmail.com>
2024-01-31 21:45:32 -0400

    Add event listeners for arithmetic operations

--- script.js
+++ script.js
@@ -15,6 +15,27 @@ const four = document.getElementById('4');
 const five = document.getElementById('5');
 const six = document.getElementById('6');
 
+// operations
+const plus = document.getElementById('+');
+const minus = document.getElementById('-');
+const times = document.getElementById('*');
+const divide = document.getElementById('/');
+
+//events
+plus.addEventListener('click', () => {
+    display.value += ''
+});
+minus.addEventListener('click', () => {
+    display.value += ''
+});
+times.addEventListener('click', () => {
+    display.value += ''
+});
+divide.addEventListener('click', () => {
+    display.value += ''
+});
+
+
 // detectamos el evento click de cada elemento
 one.addEventListener('click', () => {
     display.value += one.value;
