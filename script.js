d917b734aa8abddea27c9a2584537c29e1dbc370
Rui <48386000+Code-Rui@users.noreply.github.com>
2024-01-31 21:20:16 -0400

    benFirstCommit

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const one = document.getElementById('1');
 const two = document.getElementById('2');
 const three = document.getElementById('3');
 
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+
 // detectamos el evento click de cada elemento
 one.addEventListener('click', () => {
     display.value += one.value;
@@ -19,6 +23,20 @@ two.addEventListener('click', () => {
 three.addEventListener('click', () => {
     display.value += three.value;
 });
+
+four.addEventListener('click', () => {
+    display.value += four.value;
+});
+
+five.addEventListener('click', () => {
+    display.value += five.value;
+});
+
+six.addEventListener('click', () => {
+    display.value += six.value;
+});
+
+
 
 AC.addEventListener('click', () => {
     display.value = '';
