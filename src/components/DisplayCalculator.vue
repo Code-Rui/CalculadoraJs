0dc212d521527fced4d472faa8e1b00c1bbd9eed
jjjjose <jjjjoseignacio@gmail.com>
2024-02-16 22:32:06 -0400

    Add vue3-toastify package and implement calculator functionality

--- src/components/DisplayCalculator.vue
+++ src/components/DisplayCalculator.vue
@@ -3,12 +3,12 @@ import { storeToRefs } from "pinia";
 import { useOperationsStore } from "../stores/operations.store";
 
 const operationsStore = useOperationsStore();
-const { history, result } = storeToRefs(operationsStore);
+const { history, display } = storeToRefs(operationsStore);
 
 </script>
 <template>
   <div class="h-[70px] bg-[#202e3d] w-full rounded-sm p-2 text-right font-sans">
     <div class="text-sm text-[#d3d4d0]">{{ history.join("") }}</div>
-    <div class="text-3xl text-[#e0e1dd]">{{ result }}</div>
+    <div class="text-3xl text-[#e0e1dd]">{{ display }}</div>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import "./style.css";
 import App from "./App.vue";
 import { router } from "./routes";
 import { createPinia } from "pinia";
+import "vue3-toastify/dist/index.css";
 
 createApp(App).use(router).use(createPinia()).mount("#app");
