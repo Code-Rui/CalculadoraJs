76512505591ab4fb347fd998533b2f7589ce2b77
EmanuelQ14142 <emanuelyujra14@gmail.com>
2024-02-16 22:33:06 -0400

    add

--- src/components/BaseCalculator.vue
+++ src/components/BaseCalculator.vue
@@ -3,10 +3,40 @@ import DisplayCalculator from './DisplayCalculator.vue'
 import ButtonCalculator from './ButtonCalculator.vue'
 import BTNS from './buttons'
 import { onMounted, ref } from 'vue'
+import {useOperationsStore} from '../store/operations.store'
+import { storeToRefs } from 'pinia'
+import { toast } from 'vue3-toastify'
+const operationsStore=useOperationsStore();
+const {display}=storeToRefs(operationsStore)
+const {CONCATENATE,CLEAR_DISPLAY} = operationsStore;
+
 const buttons = ref()
 onMounted(() => {
   buttons.value = BTNS
 })
+
+function handleClick(value:string){
+
+//operadores 
+let operators =["+","-","*","/"];
+
+ 
+let displayArray=display.value.split("");
+
+let LastValue = displayArray[displayArray.length -1]
+
+  if(LastValue==="0" && operators.includes(value)) {
+    toast.error("Debe ingreasr un numero diferente a 0")
+    return;
+  }
+  CONCATENATE(value);
+ if (value=== "AC") {
+  CLEAR_DISPLAY();
+ }
+ 
+ 
+
+}
 </script>
 <template>
   <div class="col-span-2 hidden"></div>
@@ -20,6 +50,7 @@ onMounted(() => {
           :content="btn.value"
           :color="btn.color"
           :class="btn?.value === '=' && 'col-span-2'"
+          @click="handleClick(btn.value)"
         />
       </div>
     </div>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { router } from "./routes";
+import {createPinia} from "pinia"; 
+import 'vue3-toastify/dist/index.css';
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
